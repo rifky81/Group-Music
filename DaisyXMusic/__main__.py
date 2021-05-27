8c031d14ae3967c34ea099ad693cae5fc483e404
rifky81 <84845422+rifky81@users.noreply.github.com>
2021-05-27 16:04:38 +0700

    Update : main

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,4 +1,4 @@
-# DaisyXMusic (Telegram bot project)
+# Group-Music (Telegram bot project)
 # Copyright (C) 2021  Inuka Asith & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
@@ -18,8 +18,8 @@
 import requests
 from pyrogram import Client as Bot
 
-from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from DaisyXMusic.services.callsmusic import run
+from GroupMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from GroupMusic.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -31,7 +31,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="DaisyXMusic.modules"),
+    plugins=dict(root="GroupMusic.modules"),
 )
 
 bot.start()
