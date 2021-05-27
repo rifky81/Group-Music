549a7e42d40bb828313975fd7fd101d392dad445
rifky81 <84845422+rifky81@users.noreply.github.com>
2021-05-27 16:23:53 +0700

    Update : init

--- DaisyXMusic/services/converter/__init__.py
+++ DaisyXMusic/services/converter/__init__.py
@@ -3,4 +3,4 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from DaisyXMusic.services.converter.converter import convert
+from GroupMusic.services.converter.converter import convert
