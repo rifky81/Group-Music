d69d0903ba135172f18834214701701239cc7973
rifky81 <84845422+rifky81@users.noreply.github.com>
2021-05-29 10:29:34 +0700

    Membuat pm

---
+++ GroupMusic/modules/pmpermit.py
@@ -0,0 +1,30 @@
+# GroupMusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from GroupMusic.services.callsmusic.callsmusic import client as USER
+
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+    await USER.send_message(
+        message.chat.id,
+        "👋🏻 Halo kawan , saya adalah layanan asisten musik .\n\n ❗️ Rules:\n   - Tidak boleh mengobrol\n   - Tidak boleh spam chat \n\n 👉 **KIRIM LINK UNDANGAN GRUP ATAU NAMA PENGGUNA JIKA USERBOT TIDAK BISA BERGABUNG DENGAN GRUP ANDA.**\n\n ⚠️ Penolakan: Jika Anda mengirim pesan di sini berarti admin akan melihat pesan Anda dan bergabung dengan obrolan\n    - Owner @rifkyyyyyyyyyyyyyyyy.\n   - Group Support @viraltwittergrup\n\n**Managed with by rifky**\n**Modifided with by** @PacarFerdilla",
+    )
+    return
