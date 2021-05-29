425d7724e650ce55e60fefa81ba225a304e58197
rifky81 <84845422+rifky81@users.noreply.github.com>
2021-05-29 09:09:21 +0700

    Update ytsearch

--- GroupMusic/modules/ytsearch.py
+++ GroupMusic/modules/ytsearch.py
@@ -36,10 +36,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search butuh argumen!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Mencari....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
